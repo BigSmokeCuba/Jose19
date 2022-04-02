67f7af29553b6975387c02da926f4561be1202d8
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-02 09:05:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get('5100775831:AAEBSnG12VvRAhvGt875zvpA0LyavpIyzyQ','')
+BOT_TOKEN = os.environ.get('5151062359:AAE2pcB0S312wuehcIG2Joeo1Yw4cZbY_gs','')
 TG_API_ID = os.environ.get('13193029',0)
 TG_API_HASH = os.environ.get('03bca249fdbe6824b1ca6d928a2e60ee','')
 TG_ADMIN_USER = os.environ.get('BigSmoke19','')
